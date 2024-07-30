d7ad5189e896865447b976b626baa03f4f072164
LorenaSTN <lorenstn@hotmail.com>
2024-07-30 14:05:09 +0200

    title and image + some media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "../scss/components/Header.scss";
+import Title from "../images/titulo.png";
 
 function Header() {
   return (
@@ -9,7 +10,8 @@ function Header() {
         title="Haz click para volver a la página inicial"
       >
         {/* <img className="header__companyLogo" src="./images/laptop-code-solid.svg" alt="Logo proyectos molones"> */}
-        <h1 className="header__title">Proyectos molones</h1>
+        {/* <h1 className="header__title">Proyectos molones</h1> */}
+        <img className="header__subtitle" src={Title} alt="setProject" />
       </a>
       {/* <img className="logoSponsor" src="./images/adalab.png" alt="Logo Adalab"> */}
     </header>
